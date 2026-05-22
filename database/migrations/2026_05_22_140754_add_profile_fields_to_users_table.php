dede16d736927ec2fb13de2280f250b968a8348e
Romain20005 <romain.musungayi@student.ehb.be>
2026-05-22 16:27:02 +0200

    Extended users table met profile fields

---
+++ database/migrations/2026_05_22_140754_add_profile_fields_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->nullable();
+            $table->date('birthday')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->boolean('is_admin')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'username',
+                'birthday',
+                'bio',
+                'profile_picture',
+                'is_admin'
+            ]);
+        });
+    }
+};
