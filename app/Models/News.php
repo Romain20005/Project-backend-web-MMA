4b5d2a890ca4600b98ecbbeff8e3b8274585369c
Romain20005 <romain.musungayi@student.ehb.be>
2026-05-24 00:06:51 +0200

    Added relationships between users und news

--- app/Models/News.php
+++ app/Models/News.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class News extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'published_at',
+        'user_id',
+    ];
+
+    /**
+     * News belongs to one user
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Model\News;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -42,5 +44,12 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+    /**
+     * User can have many new posts
+     */
+    public function news()
+    {
+        return $this->hasMany(News::class);
     }
 }
